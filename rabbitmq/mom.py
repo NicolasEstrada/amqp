28dd54fe435528e8048ae14c89654ccd9595e438
Nicolás Estrada <nico@skout.cl>
2012-11-13 23:17:37 -0300

    - Ready to be used

--- rabbitmq/mom.py
+++ rabbitmq/mom.py
@@ -8,16 +8,56 @@ import sys
 import uuid
 import time
 import logging
-import pika
-import jsonhandler
 from random import choice
+
+import pika
+import bjoern
+from flask import Flask, jsonify, request
 
 import mq
 from mq import *
+import jsonhandler
+
+app = Flask(__name__)
 
 N_SHARDS = 10
 REQUEST_TYPES = ['AUTH', 'DB_INSERT', 'DB_UPDATE', 'DB_VIEW', 'DB_DELETE']
 SHARDS = [i for i in range(N_SHARDS)]
+
+
+def default(rkey, body):
+    print "DEFAULT: ", rkey, body
+    time.sleep(0.3)
+    return True
+
+
+def authentication(rkey, body):
+    print "AUTH: ", rkey, body
+    # INSERT + SELECT
+    time.sleep(0.3)
+    return True
+
+
+def db_store(rkey, body):
+    print "CRUD: ", rkey, body
+    # INSERT
+    time.sleep(0.2)
+    return True
+
+
+def db_shards(rkey, body):
+    print "SHARDS: ", rkey, body
+    # SELECT
+    time.sleep(0.1)
+    return True
+
+
+CALLBACKS = {
+    "default": default,
+    "authentication": authentication,
+    "db_store": db_store,
+    "db_shards": db_shards
+}
 
 
 class messageOrientedMiddleware(object):
@@ -53,44 +93,63 @@ class messageOrientedMiddleware(object):
         except:
             return False
 
+    def consume(self):
+        self.receiver.connect()
+
     def processing(self, rkey, body):
         print "Received:", rkey, body
         return True
 
 
-def authentication(rkey, body):
-    print "AUTH: ", rkey, body
-    return True
+@app.route('/')
+def index():
+    return "<h1> MOM Web Service <h1/>"
 
 
-def db_store(rkey, body):
-    print "CRUD: ", rkey, body
-    return True
+@app.route('/mom_ws/<type>', methods=['GET', 'POST'])
+def mom_ws(req_type='default'):
+    data = request.values.get('data', None)
+    if data:
+        processing = messageOrientedMiddleware(
+                durable=True, queue=req_type,
+                callback=CALLBACKS[req_type], rkeys=['test.AUTH.*'])
+        if processing.publish(req_type, jsonhandler.loads(str(data))):
+            return jsonify({"status": True})
+    else:
+        return jsonify({"status": False})
 
 
-def db_shards(rkey, body):
-    print "SHARDS: ", rkey, body
-    return True
+@app.route('/mvc_ws/<type>', methods=['GET', 'POST'])
+def mvc_ws(req_type='default'):
+    data = request.values.get('data', None)
+    if data:
+        if CALLBACKS[req_type](req_type, jsonhandler.loads(str(data))):
+            return jsonify({"status": True})
+    else:
+        return jsonify({"status": False})
+
 
 if __name__ == "__main__":
 
-    if len(sys.argv) != 2:
-        sys.exit(0)
+    bjoern.run(app, '127.0.0.1', 8088)
 
-    opt = str(sys.argv[1])
+    # if len(sys.argv) != 2:
+    #     sys.exit(0)
 
-    if opt == str(1):
-        mtest = messageOrientedMiddleware(
-            durable=True, queue='auth',
-            callback=authentication, rkeys=['test.AUTH.*'])
-        mtest.test(1000)
-    elif opt == str(2):
-        mtest = messageOrientedMiddleware(
-            durable=True, queue='db',
-            callback=db_store, rkeys=['test.DB_INSERT.*'])
-        mtest.test(1000)
-    elif opt == str(3):
-        mtest = messageOrientedMiddleware(
-            durable=True, queue='shards_all',
-            callback=db_shards, rkeys=['test.*.3'])
-        mtest.test(1000)
+    # opt = str(sys.argv[1])
+
+    # if opt == str(1):
+    #     mtest = messageOrientedMiddleware(
+    #         durable=True, queue='auth',
+    #         callback=authentication, rkeys=['test.AUTH.*'])
+    #     mtest.test(1000)
+    # elif opt == str(2):
+    #     mtest = messageOrientedMiddleware(
+    #         durable=True, queue='db',
+    #         callback=db_store, rkeys=['test.DB_INSERT.*'])
+    #     mtest.test(1000)
+    # elif opt == str(3):
+    #     mtest = messageOrientedMiddleware(
+    #         durable=True, queue='shards_all',
+    #         callback=db_shards, rkeys=['test.*.3'])
+    #     mtest.test(1000)
